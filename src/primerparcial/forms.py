1d5751900421b54025b8fd1963c87be4b6634b96
JuanDavid601 <lhdj201987@gmail.com>
2025-09-01 20:47:49 -0500

    Pulimiento Formulario

--- src/primerparcial/forms.py
+++ src/primerparcial/forms.py
@@ -1,13 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, DateField, TimeField
 from wtforms.validators import  DataRequired, length
 
 class SignupForm(FlaskForm):
     id = StringField('id', validators=[DataRequired(), length(max=32)])
     title = StringField('title', validators=[DataRequired(), length(max=30)])
     description = StringField('description', validators=[DataRequired(), length(max=150)])
-    date = SubmitField('date', validators=[DataRequired(), length(max=20)])
-    time = StringField('time', validators=[DataRequired(), length(max=100)])
+    date = DateField('date', validators=[DataRequired(), length(max=20)])
+    time = TimeField('time', validators=[DataRequired(), length(max=100)])
     location = StringField('location', validators=[DataRequired(), length(max=150)])
 
 # class PostForm(FlaskForm):
